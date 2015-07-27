f6536c32f1f8fbea902adaef0fa4368f07a8b711
Thien Truong <moments1ever+pub@gmail.com>
2015-07-27 11:32:01 -0500

    Add main.

---
+++ vendingmachine/src/main/scala/localhost/tt/vendingmachine/Main.scala
@@ -0,0 +1,19 @@
+package localhost.tt.vendingmachine
+
+object Main {
+  def main (args: Array[String]) {
+    val availableMerchandise = new VendingMachineAvailableMerchandise
+    availableMerchandise
+      .addMerchandise("A", new Merchandise("Pepsi", 2.75), 2)
+      .addMerchandise("B", new Merchandise("Coca Cola", 0.75), 5)
+      .addMerchandise("C", new Merchandise("Snicker", 2.00), 1)
+    val consoleInterface = new VendingMachineConsoleInterface(availableMerchandise)
+    availableMerchandise.displayAvailableMerchandise()
+    consoleInterface.purchaseMerchandise()
+
+    println("Hello world!")
+    println(availableMerchandise.availableMerchandise.toString())
+    println(consoleInterface.availableMerchandise.availableMerchandise.toString())
+
+  }
+}
